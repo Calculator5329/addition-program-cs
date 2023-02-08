913d89c09d5cd9c44869586f04a690b37b214946
Ethan <et2boys@hotmail.com>
2023-02-08 14:05:07 -0600

    Updated to include any operator

--- Program.cs
+++ Program.cs
@@ -10,15 +10,37 @@ namespace firstProject
     {
         static void Main(string[] args) 
         {   
-            Console.Write("Enter an integer: ");
+            Console.Write("Enter a number: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter another integer: ");
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+            Console.WriteLine(num1 + " " + op + " " + num2 + " = " + calc(num1, num2, op));
 
             Console.ReadLine();
+        }
+
+        static double calc(double num1, double num2, string op) {
+            double res = 0;
+
+            if(op == "+") {
+                res = num1 + num2;
+            }
+            else if(op == "-") {
+                res = num1 - num2;
+            }   
+            else if(op == "*") {
+                res = num1 * num2;
+            }
+            else if(op == "/") {
+                res = num1 / num2;
+            }
+
+            return res;
         }
 
     }
